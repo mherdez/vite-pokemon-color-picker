d4562a1efb401d9b505bb143de85d7f015cf39fc
Herdez <mherdez@users.noreply.github.com>
2023-03-24 15:33:18 -0600

    1010 pokemons card

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 import './style.css'
 import { showImg } from './src/components/showImg';
 
-const container = document.querySelector('#app');
-showImg();
+// const app = document.querySelector('#app');
+const app = document.querySelector('#app');
+  const container = document.createElement('div');
+  container.classList.add('container');
+  app.appendChild(container);
+
+new Array(1010).fill(null).forEach(( pokemon, index) => {
+  showImg(index + 1);
+})
+
 
 
 
--- src/components/showImg.js
+++ src/components/showImg.js
@@ -2,25 +2,30 @@ import { colorPicker, colorPalette } from './colorPicker';
 import { createImg } from './createImg';
 import { drawBox } from './drawBox';
 
-export const showImg = () => {
-  const container = document.querySelector('#app');
+export const showImg = (id) => {
+  const container = document.querySelector('.container');
 
-  const url = 'https://picsum.photos/300/500';
+  // const url = 'https://picsum.photos/300/500';
 
-  const id = Math.floor(Math.random() * 649 + 1)
+  // const id = Math.floor(Math.random() * 649 + 1)
   // const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+  const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
 
   const img = createImg(url);
-  container.appendChild(img);
+  const card = document.createElement('div');
+  card.classList.add('card');
+  container.appendChild(card);
+  card.appendChild(img);
 
   img.addEventListener('load', function () {
     const [r, g, b] = colorPicker(img);
-    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+    // document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+    card.style.backgroundColor = `rgb(${r},${g},${b})`;
 
-    const paleta = colorPalette(img);
-    const [rojo,verde,azul] = paleta[6];
-    paleta.map( ([r,g,b]) => container.appendChild(drawBox(r,g,b)))
-    img.style.backgroundColor = `rgb(${rojo},${verde},${azul})`;
+    // const paleta = colorPalette(img);
+    // const [rojo,verde,azul] = paleta[6];
+    // paleta.map( ([r,g,b]) => container.appendChild(drawBox(r,g,b)))
+    // img.style.backgroundColor = `rgb(${rojo},${verde},${azul})`;
   });
 
 };
